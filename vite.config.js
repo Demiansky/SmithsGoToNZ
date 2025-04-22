425d4ad29611169219eed42e0ce6fd3ae073ccaa
Dana Smith <technolopithicus@gmail.com>
2025-04-22 07:26:13 -0400

    Added new libraries for handling routing.

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import string from 'vite-plugin-string'; // Import the plugin
+import vitePluginString from 'vite-plugin-string'; // Import the plugin
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    string(), // Add the plugin here
+    vitePluginString(), // Add the plugin here
   ],
   server: {
     historyApiFallback: true, // Ensure React Router works with Vite
